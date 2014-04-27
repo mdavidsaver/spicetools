dafd16fef460c16d0cf8d3df5844d83791973d92
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-04-27 12:42:39 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='spice2hdf',
+setup(name='spicetools',
       version='1.0',
-      description='Convert spice output files to an hdf5 format',
+      description='Tools for working with spice simulation (ngspice mainly)',
       author='Michael Davidsaver',
       author_email='mdavidsaver@gmail.com',
-      url='http://github.com/mdavidsaver/spice2hdf',
+      url='http://github.com/mdavidsaver/spicetools',
       classifiers = [
           "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
           "Development Status :: 4 - Beta",
@@ -18,7 +18,17 @@ setup(name='spice2hdf',
           "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
       ],
       license='GPL3+',
-      packages = ['spiceview'],
-      py_modules = ['spice2hdf'],
-      scripts = ['spice2hdf','spiceviewer'],
+      packages = ['spicetools',
+                  'spicetools.bench',
+                  'spicetools.view',
+                  'spicetools.test',
+                  ],
+      package_data={
+          'spicetools.test':['*.raw']
+          },
+      scripts = ['spice2hdf',
+                 'spicebench',
+                 'spiceviewer',
+                 'spicerunner'
+                 ],
 )
